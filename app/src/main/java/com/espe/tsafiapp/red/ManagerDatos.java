869031cd835ef70c6cda1626b3f6c67c8985ccbc
moferrin <moferrin@espe.edu.ec>
2022-10-22 23:18:22 -0500

    on response

---
+++ app/src/main/java/com/espe/tsafiapp/red/ManagerDatos.java
@@ -0,0 +1,13 @@
+package com.espe.tsafiapp.red;
+
+import android.database.Cursor;
+
+public class managerDatos {
+
+    public managerDatos() {
+    }
+
+    public void enviarDatos(Cursor cursor){
+        
+    }
+}
